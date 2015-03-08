18f6644ae8dcf6f8920abf8796f8aaf54046417a
Ethan Zane Miller <ethan@code-cuts.com>
2015-03-08 09:58:24 +0000

    Add files for control bar

---
+++ src/components/controlbar/controlbar.jsx
@@ -0,0 +1,16 @@
+(function() {
+	'use strict';
+
+	var React = require('react');
+
+	var Controlbar = React.createClass({
+		render: function() {
+			return (
+				<nav id="controlbar">
+				</nav>
+			);
+		}
+	});
+
+	module.exports = Controlbar;
+}());
